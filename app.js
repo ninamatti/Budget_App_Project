87608d474fb2f8c4131f8eca6da5c0c4889d1e13
Nina Matti <ninadesireematti@gmail.com>
2020-08-15 10:37:01 +0900

    added the base module structure

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
 "use strict"
 
+let budgetController = (function() {
 
+    let x = 23;
+
+    let add = function(a) {
+        return x + a;
+    }
+
+    return {
+        publicTest: function(b) {
+            return add(b);
+        }
+    }
+
+})();
+
+
+let UIController = (function() {
+
+    // some code
+
+
+}) ();
+
+
+let controller = (function(budgetCtrl, UICtrl) {
+
+    //some code
+
+    let z = budgetCtrl.publicTest(6);
+
+    return {
+        anotherPublicTest: function() {
+            console.log(z);
+        }
+    }
+
+})(budgetController, UIController);
